b4a8728d57cc15145ecc23e0aeda1863bfc5f8cd
unknown <florezvasquezjuanmiguel@gmail.com>
2026-06-08 12:12:02 -0500

    Feat: Implementación del modo Light and Dark

--- src/app/components/layout/side-bar/side-bar.ts
+++ src/app/components/layout/side-bar/side-bar.ts
@@ -1,17 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css',
 })
 export class SideBar {
+  private themeService = inject(ThemeService);
   isCollapsed = signal(false);
+  isDarkMode = this.themeService.isDarkMode;
 
   toggleSidebar() {
     this.isCollapsed.update(value => !value);
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
   }
 }
